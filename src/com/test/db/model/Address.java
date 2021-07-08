8af411ed385b9d81bb48111587f030389485d573
mrarthurwhite <mister.arthur.white@gmail.com>
2021-07-08 13:40:49 -0400

    added address model file

---
+++ src/com/test/db/model/Address.java
@@ -0,0 +1,62 @@
+package com.test.db.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+/**
+ * 	This class contains the course details.
+ * 	
+ */
+public class Address implements java.io.Serializable {
+	
+
+
+	private long addressId;
+	private String address;
+	private Set<User> users = new HashSet<User>(0);
+
+	public Address() {
+	}
+
+	public Address(String addy) {
+		this.address = addy;
+	}
+
+	public long getAddressId() {
+		return this.addressId;
+	}
+
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String st) {
+		this.address = st;
+	}
+
+
+	/**
+	 * @return the users
+	 */
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users the users to set
+	 */
+	public void setEmployees(Set<User> users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [address=" + address +  "]";
+	}
+}
